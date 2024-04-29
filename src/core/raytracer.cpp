6aecf07a1beede7bfb312b9d676976a1aa14e043
vlazoore <victor.lazoore@epitech.eu>
2024-04-29 16:36:59 +0200

    add sfml window

--- src/core/raytracer.cpp
+++ src/core/raytracer.cpp
@@ -23,53 +23,86 @@ Color ray_color(const Ray& r) {
     return (1.0 - t) * Color(1.0, 1.0, 1.0) + t * Color(0.5, 0.7, 1.0);
 }
 
-void Raytracer::run()
+sf::VertexArray create_map(int image_width, int image_height, std::vector<std::vector<Color>> &image)
 {
-// Image
+    sf::VertexArray vertices(sf::Points, image_width * image_height);
+    for (int j = 0; j < image_height; j++) {
+        for (int i = 0; i < image_width; i++) {
+            int index = i + j * image_width;
+            vertices[index].position = sf::Vector2f(i, j);
+            vertices[index].color = sf::Color(
+                static_cast<sf::Uint8>(image[j][i].r * 255),
+                static_cast<sf::Uint8>(image[j][i].g * 255),
+                static_cast<sf::Uint8>(image[j][i].b * 255)
+            );
+        }
+    }
+    return vertices;
+}
 
+sf::VertexArray create_map(std::vector<std::vector<Color>> &image, int x, int y, int z)
+{
     auto aspect_ratio = 16.0 / 9.0;
     int image_width = 400;
-
-    // Calculate the image height, and ensure that it's at least 1.
     int image_height = int(image_width / aspect_ratio);
     image_height = (image_height < 1) ? 1 : image_height;
-
-    // Camera
-
     auto focal_length = 1.0;
     auto viewport_height = 2.0;
     auto viewport_width = viewport_height * (double(image_width)/image_height);
-    auto camera_center = Point(0, 0, 0);
-
-    // Calculate the vectors across the horizontal and down the vertical viewport edges.
+    auto camera_center = Point(x, y, z);
     auto viewport_u = Vector(viewport_width, 0, 0);
     auto viewport_v = Vector(0, -viewport_height, 0);
-
-    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
     auto pixel_delta_u = viewport_u / image_width;
     auto pixel_delta_v = viewport_v / image_height;
-
-    // Calculate the location of the upper left pixel.
-    auto viewport_upper_left = camera_center
-                             - Vector(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+    auto viewport_upper_left = camera_center - Vector(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
     auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
-    // Render
-
-
-    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
-
     for (int j = 0; j < image_height; j++) {
-        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; i++) {
             auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
             auto ray_direction = pixel_center - camera_center;
             Ray r(camera_center, ray_direction);
 
             Color pixel_color = ray_color(r);
-            pixel_color.write_color(std::cout, pixel_color);
+            image[j][i] = pixel_color;
+
         }
     }
+    sf::VertexArray vertices = create_map(image_width, image_height, image);
+    return vertices;
+}
 
-    std::clog << "\rDone.                 \n";
+void Raytracer::run()
+{
+    auto aspect_ratio = 16.0 / 9.0;
+    int image_width = 400;
+    int image_height = int(image_width / aspect_ratio);
+    int x = 0, y = 0, z = 0;
+    std::vector<std::vector<Color>> image(image_height, std::vector<Color>(image_width));
+    sf::VertexArray vertices = create_map(image, 0, 0, 0);
+    sf::RenderWindow window(sf::VideoMode(image_width, image_height), "raytracer");
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+            x++;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+            x--;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+            y++;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+            y--;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z))
+            z++;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+            z--;
+        window.clear();
+        window.draw(create_map(image, x, y, z));
+        window.display();
+    }
 }
